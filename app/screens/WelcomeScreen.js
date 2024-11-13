a5d9b0f968d44b53d44a362b4f2f70fe4a18ebc6
Cory Ross <dragong4mer404@gmail.com>
2024-11-13 16:31:22 -0500

    commented out an import statement to an empty file that was cauing issues

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, SafeAreaView, Image, View, StyleSheet, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import main.css from "./assets"
+//import main.css from "./assets"
 
 function WelcomeScreen() {
     const navigation = useNavigation();
