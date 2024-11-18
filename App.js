7bb799a15575d4c1d1c51466b14b22365eca866b
Cory Ross <dragong4mer404@gmail.com>
2024-11-18 15:09:08 -0500

    added the imported browse screen to the view stack, currently to the top so I can see the changes since I cannot get navigation working atm

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Login1 from './app/components/Login';
 import Register from './app/components/Register';
-//import login from './Screens/Assets/login';
+//import Browse from './Screens/Assets/browse';
+import BrowseScreen from './Screens/Assets/browse';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,12 +14,16 @@ export default function App() {
     console.log("App executed");
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Welcome">
+            <Stack.Navigator initialRouteName="Browse">
+                
                 <Stack.Screen name="Welcome" component={WelcomeScreen} />
                 <Stack.Screen name="Login" component={Login1} />
                 <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Browse" component={BrowseScreen} />
+                
             </Stack.Navigator>
         </NavigationContainer>
+
     );
 }
 
